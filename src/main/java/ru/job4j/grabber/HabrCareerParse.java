327a784318b572fc1532d858d9b412a604373912
Boris Sedykh <sedykh.boris@yandex.ru>
2023-12-22 18:02:05 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -13,6 +13,13 @@ public class HabrCareerParse {
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
 
+    private static String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Element description = document.select(".vacancy-description__text").first();
+        return description.text();
+    }
+
     public static void main(String[] args) throws IOException {
         for (int pageNumber = 1; pageNumber <= 5; pageNumber++) {
             System.out.printf("Parsing page %d%n", pageNumber);
@@ -20,15 +27,16 @@ public class HabrCareerParse {
             Connection connection = Jsoup.connect(fullLink);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
+            for (Element row : rows) {
                 Element dateElement = row.select(".vacancy-card__date").first().child(0);
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element linkElement = titleElement.child(0);
                 String date = dateElement.attr("datetime");
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", date, vacancyName, link);
-            });
+                String description = retrieveDescription(link);
+                System.out.printf("%s %s %s %s%n", date, vacancyName, link, description);
+            }
         }
     }
 }
